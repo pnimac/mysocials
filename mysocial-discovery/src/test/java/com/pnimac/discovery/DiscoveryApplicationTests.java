a0a6d859f4eddfbfce60902ba95269e656714e90
“pnimac” <poornima.chauhan.2019@gmail.com>
2024-06-11 20:34:09 +1000

    Discovery Service Initial commit

---
+++ mysocial-discovery/src/test/java/com/pnimac/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,11 @@
+package com.pnimac.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiscoveryApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
